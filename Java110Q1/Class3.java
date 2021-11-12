a57279d8b33c19d0d1368babdb97ca5f36b284b1
黃俊哲 <91860942+109b15222@users.noreply.github.com>
2021-11-12 13:06:20 +0800

    Add files via upload

---
+++ Java110Q1/Class3.java
@@ -0,0 +1,42 @@
+class Data
+{
+	private String name;
+	private Test score;
+	
+	class Test
+	{
+		int english,math;
+		public Test(int eng,int m)
+		{
+			english=eng;
+			math=m;
+		}
+	public double avg()
+	{
+		return ((english+math)/2.0);
+	}
+	}
+	public Data(String n,int e,int m)
+	{
+		name=n;
+		score=new Test(e,m);
+	}
+	public void show()
+	{
+		System.out.println("name="+name);
+		System.out.println("english="+score.english);
+		System.out.println("math="+score.math);
+		System.out.println("avg="+score.avg());
+	}
+}
+
+public class Class3 {
+	public static void main(String args[])
+	{
+		Data stu=new Data("Annie",85,92);
+		stu.show();
+		Data stu2=new Data("Brian",77,56);
+		stu2.show();
+	}
+
+}
